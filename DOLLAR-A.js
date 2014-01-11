e9e79b86e452209379dead2b23a9c7554f129cb9
Darren Embry <dse@webonastick.com>
2014-01-10 21:41:20 -0500

    make jslint happy.

--- DOLLAR-A.js
+++ DOLLAR-A.js
@@ -5,11 +5,20 @@
 
 // https://github.com/sstephenson/prototype/blob/e3d5200cef3236b257ddcfb1eb3eabc5c586bedd/src/prototype/lang/array.js#L60
 function $A(iterable) {
-	if (!iterable) return [];
+	var length, results;
+	if (!iterable) {
+		return [];
+	}
 	// Safari <2.0.4 crashes when accessing property of a node list with property accessor.
 	// It nevertheless works fine with `in` operator, which is why we use it here
-	if ('toArray' in Object(iterable)) return iterable.toArray();
-	var length = iterable.length || 0, results = new Array(length);
-	while (length--) results[length] = iterable[length];
+	if ('toArray' in Object(iterable)) {
+		return iterable.toArray();
+	}
+	length = iterable.length || 0;
+	results = new Array(length);
+	while (length--) {
+		results[length] = iterable[length];
+	}
 	return results;
 }
+
