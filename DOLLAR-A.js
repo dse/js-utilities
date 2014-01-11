a18fc0b1a69485eedd915ce32b32b6865f440ec5
Darren Embry <dse@webonastick.com>
2014-01-10 21:39:17 -0500

    $A taken from prototype.js.

---
+++ DOLLAR-A.js
@@ -0,0 +1,15 @@
+/*jslint browser: true, sloppy: true */
+//-----------------------------------------------------------------------------
+// Lines above are for jslint, the JavaScript verifier.  http://www.jslint.com/
+//-----------------------------------------------------------------------------
+
+// https://github.com/sstephenson/prototype/blob/e3d5200cef3236b257ddcfb1eb3eabc5c586bedd/src/prototype/lang/array.js#L60
+function $A(iterable) {
+	if (!iterable) return [];
+	// Safari <2.0.4 crashes when accessing property of a node list with property accessor.
+	// It nevertheless works fine with `in` operator, which is why we use it here
+	if ('toArray' in Object(iterable)) return iterable.toArray();
+	var length = iterable.length || 0, results = new Array(length);
+	while (length--) results[length] = iterable[length];
+	return results;
+}
