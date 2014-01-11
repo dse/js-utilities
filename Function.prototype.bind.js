906957b6215aed05fd9f6c2236147f1f5bff42f8
Darren Embry <dse@webonastick.com>
2014-01-10 21:51:47 -0500

    jslint happy

--- Function.prototype.bind.js
+++ Function.prototype.bind.js
@@ -7,26 +7,25 @@
 // https://github.com/sstephenson/prototype/blob/e3d5200cef3236b257ddcfb1eb3eabc5c586bedd/src/prototype/lang/function.js
 if (!Function.prototype.bind) {
 	Function.prototype.bind = function (context) {
-		if (arguments.length < 2 && Object.isUndefined(arguments[0]))
+		var nop, __method, args, bound;
+		if (arguments.length < 2 && Object.isUndefined(arguments[0])) {
 			return this;
-
-		if (!Object.isFunction(this))
+		}
+		if (!Object.isFunction(this)) {
 			throw new TypeError("The object is not callable.");
-
-		var nop = function() {};
-		var __method = this, args = slice.call(arguments, 1);
-
-		var bound = function() {
+		}
+		nop = function() {};
+		__method = this;
+		args = slice.call(arguments, 1);
+		bound = function() {
 			var a = merge(args, arguments);
 			// Ignore the supplied context when the bound function is called with
 			// the "new" keyword.
 			var c = this instanceof bound ? this : context;
 			return __method.apply(c, a);
 		};
-
 		nop.prototype   = this.prototype;
 		bound.prototype = new nop();
-
 		return bound;
 	};
 }
